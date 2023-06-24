2075a7409db3bfb43f6c7d1bdc5a95e875bf6c22
Anatolii <agryzlov.mosbrew@gmail.com>
2023-06-24 22:45:25 +0300

    looks like base game model is finished
    now let's try to incorporate it in our view

--- field_model.h
+++ field_model.h
@@ -4,10 +4,11 @@
 #include <vector>
 #include <cassert>
 #include <algorithm>
+#include <utility>
 
 struct GameField {
 public:
-    GameField(size_t width, size_t height) : width(width), height(height), field(std::vector<int>(width*height)) {}
+    GameField(size_t width, size_t height) : width(width), height(height), field(std::vector<int>(width*height, 0)) {}
     size_t set(size_t r, size_t c, size_t val) {
         assert((val == 1) || (val ==0) );
         field.at(r*this->width + c) = val;
@@ -18,14 +19,18 @@ public:
         return field.at(r*this->width + c);
     }
 
+    std::pair<size_t, size_t> get_limits() const {
+        return std::make_pair(width, height);
+    }
+
     size_t switch_cell(size_t r, size_t c) {
         return field.at(r*width + c) ^ 1;
     }
 
     void switch_cells(size_t r, size_t c) {
-        for (size_t i = std::clamp<size_t>(r - 1, 0, height); i < std::clamp<size_t>(r+1, 0, width); ++i) {
-            for (size_t j = std::clamp<size_t>(r - 1, 0, height); j < std::clamp<size_t>(r+1, 0, width); ++j) {
-
+        for (size_t i = std::clamp<size_t>(r - 1, 0, height); i <= std::clamp<size_t>(r+1, 0, height); ++i) {
+            for (size_t j = std::clamp<size_t>(c - 1, 0, width); j <= std::clamp<size_t>(c+1, 0, width); ++j) {
+                switch_cell(i, j);
             }
         }
     }
